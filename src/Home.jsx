2755831a5d7fa02c71f438b2e4ee63db6d7e990d
Dicky Nursalim <diki.hma@gmail.com>
2026-04-27 07:52:03 +0700

    add card product

--- src/Home.jsx
+++ src/Home.jsx
@@ -57,6 +57,37 @@ function Home(){
         }
     ])
 
+    const [products , setProducts] = useState([
+        {
+            code : 'tentang_kamu',
+            image : 'tentang-kamu.png',
+            title : 'Tentang Kamu',
+            author : 'Tere Liye',
+            harga : 80000
+        },
+        {
+            code : 'pergi',
+            image : 'pergi.jpg',
+            title : 'Pergi',
+            author : 'Tere Liye',
+            harga : 98000
+        },
+        {
+            code : 'nebula',
+            image : 'nebula.jpg',
+            title : 'Nebula',
+            author : 'Tere Liye',
+            harga : 102000
+        },
+        {
+            code : 'moon',
+            image : 'moon.jpg',
+            title : 'Moon',
+            author : 'Tere Liye',
+            harga : 110000
+        }
+    ])
+
     const toggleDropDown = (code) => {
         const newFilter = []
         for (const fl of filterLeft){
@@ -87,6 +118,24 @@ function Home(){
         }
 
         setFilterRight(newFilter)
+    }
+    const numberFormat = (number) =>{
+        
+        let str = number.toString();
+        let parts = str.split('.');
+        let integerPart = parts[0];
+        let decimalPart = parts[1] ? '.' + parts[1] : '';
+        
+        
+        let formatted = '';
+        for (let i = integerPart.length - 1, count = 0; i >= 0; i--, count++) {
+            if (count > 0 && count % 3 === 0) {
+            formatted = '.' + formatted;
+            }
+            formatted = integerPart[i] + formatted;
+        }
+        
+        return formatted + decimalPart;
     }
     return (
 
@@ -131,7 +180,25 @@ function Home(){
                 </div>
                 <div className="content-data">
                     <div className="cd-item">
-                        
+                        <div className="cd-item-product">
+                            {products.map((v, k) => (
+                                <div className="cdi-card" key={k}>
+                                    <div className="cdic-image">
+                                        <img src={`/assets/images/${v.image}`}/>
+                                    </div>
+                                    <div className="cdic-title">
+                                        {v.title}
+                                    </div>
+                                    <div className="cdic-author">
+                                        {v.author}
+                                    </div>
+                                    <div className="cdic-price">
+                                        Rp. {numberFormat(v.harga)}
+                                    </div>
+                                </div>
+                            ))}
+                            
+                        </div>
                     </div>
                 </div>
             </div>
